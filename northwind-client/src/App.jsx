c42b94f65d185bd07e1c6e482c47b05735abb60b
Anis <anisictiu@gmail.com>
2025-08-13 20:51:08 +0600

    Init Layout

---
+++ northwind-client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Layout from './components/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
+
+function App() {
+
+  return (
+    <>
+<BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ northwind-client/src/components/Layout.jsx
@@ -0,0 +1,23 @@
+// src/components/Layout.jsx
+import Footer from "./Footer";
+import { useState } from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+export default function Layout({ children }) {
+
+  return (
+    <>
+      <Header />
+      <div id="layoutSidenav">
+        <Sidebar/>
+      </div>
+      <div id="layoutSidenav_content">
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
